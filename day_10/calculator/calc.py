580c8d0511e83a44b7bc3a12283aa6107adad28e
Andrew <and.wroe@gmail.com>
2024-01-20 18:23:44 +0300

    working calc

--- day_10/calculator/calc.py
+++ day_10/calculator/calc.py
@@ -1,4 +1,5 @@
 # calculator
+from art import calculator_pic
 
 def calculator():
     def add(n1, n2):
@@ -19,7 +20,8 @@ def calculator():
         '*': multiply,
         '/': divide
     }
-
+    
+    calculator_pic()
 
     num1 = float(input("What's the first number? : "))
     for symbol in operations:
@@ -30,25 +32,32 @@ def calculator():
         if operation in operations:
             num2 = float(input("What's the next number? : "))
             result = operations[operation](num1, num2)
-            continue_calc = input(f"type 'y' to continue calculating with {result} or any other key to exit : ")
-            print(continue_calc.lower())
+            continue_calc = input(f"type 'y' to continue calculating with {result} or n to exit : ")
             while continue_calc.lower() != 'y' and continue_calc.lower() != 'n':
-                continue_calc = input('I did not understand, please enter y or n : ')
+                continue_calc = input('I did not understand, please enter y or n : ').lower()
             if continue_calc == 'y':
                 num1 = result
                 operation = input('What operation do you want to perform? : ')
                 num2 = float(input("What's the next number? : "))
                 result = operations[operation](num1, num2)
                 print(f'{num1} {operation} {num2} = {result}')
-                continue_calc = input(f"type 'y' to continue calculating with {result} or any other key to exit : ")
+                continue_calc = input(f"type 'y' to continue calculating with {result} or n to exit : ")
                 if continue_calc == 'y':
                     keep_running = True
                 else:
+                    if input("Press 'y' to start a fresh calculation or 'n' to exit: ").lower() == 'n':
+                        keep_running = False
+                    else:
+                        keep_running = True
+                        calculator()
+                    print(f'{num1} {operation} {num2} = {result}')
                     keep_running = False
             elif input("Press 'y' to start a fresh calculation or 'n' to exit: ").lower() == 'n':
                 keep_running = False
                 print(f'{num1} {operation} {num2} = {result}')
-        else:
-            print('Bye!')
+                print('Bye!')
+            else:
+                calculator()
+
 calculator()
 
